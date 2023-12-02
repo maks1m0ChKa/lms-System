0ede200c16e08ac8c35347c60cb91698c3f7ce28
Maxim Zamirailov <maksimzamirajlov@gmail.com>
2023-12-02 17:35:24 +0600

    any commit2

--- src/main/java/brain/core/User/userService/UserServeceImplements.java
+++ src/main/java/brain/core/User/userService/UserServeceImplements.java
@@ -3,5 +3,7 @@ package brain.core.User.userService;
 //грубо говоря зарисовки
 //в сервисе создаю интерфейс и туда пихаю название всех методов  какие у меня будет иметь сам юзер
 public class UserServeceImplements implements UserService {
+    public static void main(String[] args) {
 
+    }
 }
